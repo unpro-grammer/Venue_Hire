84f44ea5b073bbbc0cdc4e7e58541c9b5753578f
Grace <gxu660@aucklanduni.ac.nz>
2024-03-17 12:44:52 +1300

    added constructor and get methods for fields of venue class

---
+++ src/main/java/nz/ac/auckland/se281/Venue.java
@@ -0,0 +1,26 @@
+package nz.ac.auckland.se281;
+
+public class Venue {
+  private String venueName;
+  private String venueCode;
+  private String capacityInput;
+  private String hirefeeInput;
+
+  public Venue() {}
+
+  public String getVenueName() {
+    return venueName;
+  }
+
+  public String getVenueCode() {
+    return venueCode;
+  }
+
+  public String getCapacityInput() {
+    return capacityInput;
+  }
+
+  public String getHireFeeInput() {
+    return hirefeeInput;
+  }
+}
